7b7896617fbdbd6cdcfeb1238a25d7d88768f8b6
Vojtech Bartos <vojta@sleepio.com>
2014-12-05 15:00:14 +0000

    Models: adding createdAt and updatedAt fields to base model

--- smsgw/models/__init__.py
+++ smsgw/models/__init__.py
@@ -2,12 +2,19 @@
 # http://google-styleguide.googlecode.com/svn/trunk/pyguide.html
 
 from smsgw.extensions import db
+from datetime import datetime
+from sqlalchemy.sql.expression import text
 from sqlalchemy.ext.declarative import AbstractConcreteBase
 
 
 class BaseModel(AbstractConcreteBase, db.Model):
     
     __table_args__ = {'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8'}
+
+    createdAt = db.Column(db.TIMESTAMP, default=datetime.utcnow, 
+                          server_default=text('CURRENT_TIMESTAMP'))
+    updatedAt = db.Column(db.TIMESTAMP, default=datetime.utcnow, 
+                          onupdate=datetime.utcnow)
 
     def to_dict(self, properties):
         return {key: getattr(self, key) for key in properties}
