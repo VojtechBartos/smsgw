93dd114fcd894203dd33d34a95320fff70e4c102
Vojta Bartos <me@vojtechbartos.cz>
2015-02-08 16:27:27 +0100

    BaseModel: added new helper method get_one to base model

--- smsgw/models/base.py
+++ smsgw/models/base.py
@@ -5,10 +5,11 @@ from smsgw.extensions import db
 from datetime import datetime
 from sqlalchemy.sql.expression import text
 from sqlalchemy.ext.declarative import AbstractConcreteBase
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 
 class BaseModel(AbstractConcreteBase, db.Model):
-    
+
     __table_args__ = {'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8'}
 
     def to_dict(self, properties=None):
@@ -18,6 +19,18 @@ class BaseModel(AbstractConcreteBase, db.Model):
         for key in data:
             if hasattr(self, key):
                 setattr(self, key, data[key])
+
+    @classmethod
+    def get_one(cls, **kwargs):
+        """
+        Find item by kwargs
+        """
+        try:
+            instance = cls.query.filter_by(**kwargs).one()
+        except (NoResultFound, MultipleResultsFound):
+            instance = None
+
+        return instance
 
     @classmethod
     def get_or_create(cls, _latest=True, **kwargs):
@@ -33,8 +46,8 @@ class BaseModel(AbstractConcreteBase, db.Model):
 
 class DateMixin(object):
 
-    createdAt = db.Column(db.TIMESTAMP, default=datetime.utcnow, 
+    createdAt = db.Column(db.TIMESTAMP, default=datetime.utcnow,
                           server_default=text('CURRENT_TIMESTAMP'))
 
-    updatedAt = db.Column(db.TIMESTAMP, default=datetime.utcnow, 
+    updatedAt = db.Column(db.TIMESTAMP, default=datetime.utcnow,
                           onupdate=datetime.utcnow)
