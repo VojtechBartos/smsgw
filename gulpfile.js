2517a04f279703bd895051518742b315f2f72c8d
Vojta Bartos <me@vojtechbartos.cz>
2015-03-28 16:37:18 +0100

    Gulpfile: replacing coffee script version by ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,44 @@
-require('coffee-script/register');
-require('./gulpfile.coffee');
+/**
+ * https://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
+ */
+"use strict";
+
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var isDevelopment = true;
+
+/**
+ * Webpack
+ */
+gulp.task('webpack', function() {
+  gulp
+    .src('./smsgw/static/src/app.js')
+    .pipe(webpack({
+      cache: (isDevelopment),
+      watch: (isDevelopment),
+      devtool: (isDevelopment) ? 'eval' : '',
+      output: {
+        filename: "js/[name].js"
+      },
+      module: {
+        loaders: [
+          {
+            test: /\.coffee$/,
+            exclude: /node_modules/,
+            loaders: ['coffee', 'cjsx']
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest('./smsgw/static/build/'));
+});
+
+/**
+ * Default task
+ */
+gulp.task('default', ['webpack']);
