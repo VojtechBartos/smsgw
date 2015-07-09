4006909ce95fcad5134abfe5d4ab9975dbd3fc6c
Vojta Bartos <me@vojtechbartos.cz>
2015-07-09 09:38:25 +0200

    OutboxResource: fixing tests
    - wrong import
    - wrong urns

--- smsgw/resources/outbox/api_test.py
+++ smsgw/resources/outbox/api_test.py
@@ -2,18 +2,18 @@
 # http://google-styleguide.googlecode.com/svn/trunk/pyguide.html
 
 from smsgw.tests import SmsgwIntegrationTestCase
-from smsgw.resources.outbox import datatests
+from smsgw.resources.outbox import datasets
 
 
 class OutboxResourceTest(SmsgwIntegrationTestCase):
 
-    POST_URN = GET_URN = '/api/1.0/outbox/'
+    POST_URN = GET_URN = '/api/1.0/users/@me/outbox/'
 
     def test_get_endpoint(self):
         """ Testing user GET endpoint """
 
         # no messages in outbox
-        res = self.GET(self.GET_URN)
+        res = self.get(self.GET_URN)
         data = res.json['data']
         self.assert200(res)
         self.assertEqual(len(data), 0)
