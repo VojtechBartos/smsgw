3587abc131cfd71db775b3ee48b527297e097d88
Vojta Bartos <me@vojtechbartos.cz>
2015-07-09 09:27:15 +0200

    OutboxResource: added basic post api functionality

--- smsgw/resources/outbox/api.py
+++ smsgw/resources/outbox/api.py
@@ -8,7 +8,7 @@ from flask.ext.classy import FlaskView, route
 from smsgw.models import Outbox
 from smsgw.lib.utils import response
 from smsgw.resources import decorators
-# from smsgw.resources.users.schemas import post, put
+from smsgw.resources.outbox.schemas import post
 from smsgw.resources.error.api import ErrorResource
 from smsgw.extensions import db
 
@@ -28,14 +28,28 @@ class OutboxResource(FlaskView):
         return response([message.to_dict() for message in user.outbox.all()])
 
 
+    @decorators.jsonschema_validate(post.schema)
+    def post(self):
+        """
+        Creating new message in outbox
+        """
+        user = kwargs.get('user')
+        message = request.json.get('message')
+        phone_number = request.json.get('phoneNumber')
+        outbox = Outbox.send(phone_number=phone_number,
+                             message=message)
+        
+        return response(outbox.to_dict(), status_code=201)
+
+
     @route('/<int:outbox_id>/', methods=['DELETE'])
     @decorators.auth()
     def delete(self, outbox, **kwargs):
         """
         Delete outbox message
-        """        
-        paylod = deepcopy(outbox.to_dict())
+        """
+        payload = deepcopy(outbox.to_dict())
         db.session.delete(outbox)
         db.session.commit()
 
-        return response(paylod)
+        return response(payload)
