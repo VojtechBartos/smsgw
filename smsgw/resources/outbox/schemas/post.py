1202c712662e78e5f2e800526f6108a234b3362e
Vojta Bartos <me@vojtechbartos.cz>
2015-07-28 10:55:49 +0200

    OutboxResource: added external endpoint and refactored post and delete endpoint

--- smsgw/resources/outbox/schemas/post.py
+++ smsgw/resources/outbox/schemas/post.py
@@ -7,7 +7,7 @@ schema = {
     "description": "Schema for the outbox POST endpoint",
     "type": "object",
     "method": "POST",
-    "required": ["message", "phoneNumber"],
+    "required": ["message"],
     "additionalProperties": False,
     "properties": {
         "message": {
@@ -15,9 +15,30 @@ schema = {
             "format": "email",
             "minLength": 1
         },
-        "phoneNumber": {
+        "phoneNumbers": {
+            "type": "array",
+            "items": {
+                "type": "string",
+                "pattern": patterns.PHONE_NUMBER
+            }
+        },
+        "tags": {
+            "type": "array",
+            "items": {
+                "type": "string",
+                "pattern": patterns.UUID
+            }
+        },
+        "contacts": {
+            "type": "array",
+            "items": {
+                "type": "string",
+                "pattern": patterns.UUID
+            }
+        },
+        "send": {
             "type": "string",
-            "pattern": patterns.PHONE_NUMBER
+            "pattern": patterns.DATE_TIME_SECONDS
         }
     }
 }
