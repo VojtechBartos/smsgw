07d69c18ca1409f030f25845fddb6b80b2a3e7d5
Vojta Bartos <me@vojtechbartos.cz>
2015-07-28 10:47:31 +0200

    SentItemModel: added new field group and to_dict method
    - also added relation to contact model
    - removed not necessary whitecaps

--- smsgw/models/sentitem.py
+++ smsgw/models/sentitem.py
@@ -18,8 +18,10 @@ class SentItem(BaseModel):
 
     id = db.Column(mysql.INTEGER(10, unsigned=True), primary_key=True)
     userId = db.Column(mysql.INTEGER(10, unsigned=True), ForeignKey('user.id'))
-    applicationId = db.Column(mysql.INTEGER(10, unsigned=True), 
+    applicationId = db.Column(mysql.INTEGER(10, unsigned=True),
                               ForeignKey('application.id'))
+
+    group = db.Column(db.String(8))
 
     creator = db.Column(mysql.TEXT, nullable=False)
     phone = db.Column(db.String(255))
@@ -28,16 +30,16 @@ class SentItem(BaseModel):
     smscNumber = db.Column(db.String(20), nullable=False)
 
     coding = db.Column(db.Enum('Default_No_Compression','Unicode_No_Compression',
-                                '8bit','Default_Compression','Unicode_Compression'), 
+                                '8bit','Default_Compression','Unicode_Compression'),
                        server_default='Default_No_Compression', nullable=False)
     text = db.Column(mysql.TEXT, nullable=False)
     textEncoded = db.Column(mysql.TEXT, nullable=False)
     udh = db.Column(mysql.TEXT, nullable=False)
     klass = db.Column('class', mysql.INTEGER, server_default='-1', nullable=False)
-    
+
     status = db.Column(db.Enum('SendingOK','SendingOKNoReport','SendingError',
                                 'DeliveryOK','DeliveryFailed','DeliveryPending',
-                                'DeliveryUnknown','Error'), 
+                                'DeliveryUnknown','Error'),
                        server_default='SendingOK', nullable=False)
     statusError = db.Column(mysql.INTEGER, nullable=False, server_default='-1')
 
@@ -46,16 +48,50 @@ class SentItem(BaseModel):
     sequencePosition = db.Column(mysql.INTEGER, server_default='1', nullable=False)
 
     delivery = db.Column(db.TIMESTAMP)
-    send = db.Column(db.TIMESTAMP, default=datetime.utcnow)
+    sent = db.Column("send", db.TIMESTAMP, default=datetime.utcnow)
+
+    contact = db.relationship(
+        'Contact',
+        primaryjoin="Contact.phoneNumber==SentItem.destinationNumber",
+        foreign_keys=[destinationNumber],
+        uselist=True
+    )
+
     created = db.Column(
-        db.TIMESTAMP, default=datetime.utcnow, 
+        db.TIMESTAMP, default=datetime.utcnow,
         server_default=dbtext('CURRENT_TIMESTAMP')
     )
     updated = db.Column(
-        db.TIMESTAMP, default=datetime.utcnow, 
+        db.TIMESTAMP, default=datetime.utcnow,
         onupdate=datetime.utcnow
     )
 
+
+    def to_dict(self, properties=None):
+        """
+        To dictionary
+        :param properties: {list} of required properties
+        :return: {dict}
+        """
+        dict = {
+            'id': self.id,
+            'destinationNumber': self.destinationNumber,
+            'contact': self.contact.to_dict() if self.contact else None,
+            'status': self.status,
+            'application': self.application.to_dict() if self.application \
+                                                      else None,
+            'text': self.text,
+            'send': self.sent.isoformat(sep=' ') if self.sent else None,
+            'created': self.created.isoformat(sep=' ') if self.created \
+                                                       else None,
+            'updated': self.updated.isoformat(sep=' ') if self.updated \
+                                                       else None
+        }
+
+        if properties is None:
+            properties = dict.keys()
+
+        return {key: dict.get(key) for key in properties}
 
 Index('sentitem_date', SentItem.delivery)
 Index('sentitem_tpmr', SentItem.tpmr)
