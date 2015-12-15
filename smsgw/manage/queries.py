7edb47e3266a978cfae7b57c1002238f3023c6db
Vojta Bartos <me@vojtechbartos.cz>
2015-12-15 09:49:18 +0100

    Added setting up gammu db version as part of the sync manage command

--- smsgw/manage/queries.py
+++ smsgw/manage/queries.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # http://google-styleguide.googlecode.com/svn/trunk/pyguide.html
 
+from flask import current_app as app
 from flask.ext.script import Command, Option
+
+from smsgw.models import Gammu
 from smsgw.core import db
 
 
@@ -18,6 +21,11 @@ class SyncCommand(Command):
             db.drop_all()
         # check if tables exist, if not create them
         db.create_all()
+
+        # inserting necessary gammu database version
+        Gammu.update_version(version=app.config['GAMMU_DATABASE_VERSION'],
+                             library_version=app.config['GAMMU_VERSION'])
+
         # actually do sql actions
         db.session.commit()
         print 'Done'
