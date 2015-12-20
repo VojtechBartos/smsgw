3b85c2b305209b0fe65d78a357d9e4d9937a16f6
Vojta Bartos <me@vojtechbartos.cz>
2015-12-20 16:15:15 +0100

    WSGI using app instance from module instead of factory method

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 # http://google-styleguide.googlecode.com/svn/trunk/pyguide.html
 
-from smsgw import factory
+from smsgw import app
 
 if __name__ == "__main__":
-    app = factory.create_app()
     app.run(host='0.0.0.0', port=5000)
